72b02afdf2d8cccc6c82cf863ddd7296c68d29fa
Nicholas Chen <nicholaschen@google.com>
2015-01-21 14:41:16 -0500

    "push new set of pydocs for service account change"

--- googleads/common.py
+++ googleads/common.py
@@ -28,7 +28,7 @@ import yaml
 import googleads.errors
 import googleads.oauth2
 
-VERSION = '3.0.0'
+VERSION = '3.0.1'
 _COMMON_LIB_SIG = 'googleads/%s' % VERSION
 _PROXY_YAML_KEY = 'proxy_info'
 _PYTHON_VERSION = 'Python/%d.%d' % (sys.version_info[0], sys.version_info[1])
