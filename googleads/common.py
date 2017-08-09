4d9c239a40201d086a311ece1c0a36bc347fda0c
Mark Saniscalchi <msaniscalchi@users.noreply.github.com>
2017-08-09 15:39:43 -0400

    AdWords v201708 Release

--- googleads/common.py
+++ googleads/common.py
@@ -66,7 +66,7 @@ _DEPRECATED_VERSION_TEMPLATE = (
     'compatibility with this library, upgrade to Python 2.7.9 or higher.')
 
 
-VERSION = '6.0.0'
+VERSION = '6.1.0'
 _COMMON_LIB_SIG = 'googleads/%s' % VERSION
 _LOGGING_KEY = 'logging'
 _HTTP_PROXY_YAML_KEY = 'http_proxy'
