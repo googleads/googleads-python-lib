6e3edd5fd8e8e90f8533182069017295a9823843
David Wihl <davidwihl@users.noreply.github.com>
2019-05-16 14:34:50 -0400

    Releasing v19.0.0

---
+++ examples/ad_manager/v201905/product_package_service/create_product_packages.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This example creates a product package.
+
+To determine which product packages exist, run get_all_product_packages.py
+"""
+
+# Import appropriate modules from the client library.
+import random
+
+from googleads import ad_manager
+
+# Set the ID of the rate card to associate with the product package.
+RATE_CARD_ID = 'INSERT_RATE_CARD_ID_HERE'
+
+
+def main(client):
+  product_package_service = client.GetService('ProductPackageService',
+                                              version='v201905')
+
+  # Create a local product package.
+  product_package = {
+      'name': 'Product Package #%d' % random.randint(0, 100000000),
+      'rateCardIds': [RATE_CARD_ID]
+  }
+
+  result = product_package_service.createProductPackages([product_package])
+
+  for new_product_package in result:
+    print ('A product package with ID %d and name "%s" was created.' %
+           (new_product_package['id'], new_product_package['name']))
+
+
+if __name__ == '__main__':
+  # Initialize client object.
+  ad_manager_client = ad_manager.AdManagerClient.LoadFromStorage()
+  main(ad_manager_client)
---
+++ examples/ad_manager/v201905/product_service/update_products.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This example updates a product's notes.
+
+To determine which products exist, run get_all_products.py.
+
+"""
+
+
+# Import appropriate modules from the client library.
+from googleads import ad_manager
+
+PRODUCT_ID = 'INSERT_PRODUCT_ID_HERE'
+
+
+def main(client, product_id):
+  # Initialize appropriate service.
+  product_service = client.GetService('ProductService', version='v201905')
+
+  # Create statement object to select a single product by an ID.
+  statement = (ad_manager.StatementBuilder(version='v201905')
+               .Where('id = :id')
+               .WithBindVariable('id', long(product_id))
+               .Limit(1))
+
+  # Get products by statement.
+  response = product_service.getProductsByStatement(statement.ToStatement())
+
+  if 'results' in response and len(response['results']):
+    # Update each local product object by changing its notes.
+    updated_products = []
+    for product in response['results']:
+      product['notes'] = 'Product needs further review before activation.'
+      updated_products.append(product)
+
+    # Update products remotely.
+    products = product_service.updateProducts(updated_products)
+
+    # Display results.
+    if products:
+      for product in products:
+        print ('Product with id "%s", name "%s", and '
+               'notes "%s" was updated.'
+               % (product['id'], product['name'], product['notes']))
+    else:
+      print 'No products were updated.'
+  else:
+    print 'No products found to update.'
+
+
+if __name__ == '__main__':
+  # Initialize client object.
+  ad_manager_client = ad_manager.AdManagerClient.LoadFromStorage()
+  main(ad_manager_client, PRODUCT_ID)
